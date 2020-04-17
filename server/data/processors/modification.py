0fb675b2eb10dcb23d3fc45d58919ef5cbf37e03
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-04-17 12:30:14 +0200

    adjusted modifier classes to match other code better

--- server/data/processors/modification.py
+++ server/data/processors/modification.py
@@ -64,7 +64,7 @@ class PageRanker(Modifier):
         for node in graph.nodes:
             node.wgts[models.NodeType.PAGERANK] = ranks[node.id]
 
-    def __init__(self, *args, num_iterations: int = 100, d: float = 0.85, normalize=True, **kwargs):
+    def __init__(self, *args, num_iterations: int = None, d: float = None, normalize=None, **kwargs):
         """
         Returns base_weight iff split_a and split_b are part of the same comment.
         :param args:
@@ -74,11 +74,12 @@ class PageRanker(Modifier):
         :param kwargs:
         """
         super().__init__(*args, **kwargs)
-        self.num_iterations = num_iterations
-        self.d = d
-        self.normalize = normalize
+        self.num_iterations = self.conf_getint('num_iterations', num_iterations)
+        self.d = self.conf_getint('d', d)
+        self.normalize = self.conf_getboolean('normalize', normalize)
 
-        logger.debug(f'{self.__class__.__name__} initialised')
+        logger.debug(f'{self.__class__.__name__} initialised with '
+                     f'{self.num_iterations=}, {self.d=} and {self.normalize=}')
 
     @classmethod
     def short_name(cls) -> str:
@@ -99,78 +100,148 @@ def build_edge_dict(graph: models.Graph):
     return dic
 
 
-class EdgeFilter(Modifier):
-    def __init__(self, *args, thresholds, **kwargs):
+# class EdgeFilter(Modifier):
+#     def __init__(self, *args, thresholds, **kwargs):
+#         """
+#         Returns base_weight iff split_a and split_b are part of the same comment.
+#         :param args:
+#         :param kwargs:
+#         """
+#         super().__init__(*args, **kwargs)
+#         # fixme: do this in a way that mathces config files
+#         self.thresholds = {models.EdgeType.SAME_COMMENT: 0.7,
+#                            models.EdgeType.SAME_ARTICLE: 0.8,
+#                            models.EdgeType.SIMILARITY: 0.9,
+#                            models.EdgeType.SAME_GROUP: 0.1,
+#                            models.EdgeType.REPLY_TO: 0.8}
+#
+#         logger.debug(f'{self.__class__.__name__} initialised with '
+#                      f'{self.thresholds=}')
+#
+#     @classmethod
+#     def short_name(cls) -> str:
+#         return 'ef'
+#
+#     def modify(self, graph: models.Graph) -> models.Graph:
+#         def within_thresholds(wgt_list: List[models.EdgeWeightType], thresholds: Dict[models.EdgeType, float]):
+#             for wgt in wgt_list:
+#                 if wgt[0] < thresholds[wgt[1]]:
+#                     return False
+#             return True
+#
+#         graph.edges = [edge for edge in graph.edges if within_thresholds(edge.wgts, thresholds=self.thresholds)]
+#         return graph
+#     # def remove_edges(self, textual=0.8, structural=0.96, temporal=7200):
+#     #     # self.edges = [edge for edge in self.edges if edge.weights_bigger_as_threshold()]
+#     #     self.edges = [edge for edge in self.edges
+#     #                   if edge.weights["textual"] >= textual
+#     #                   and edge.weights["structural"] >= structural
+#     #                   and edge.weights["temporal"] <= temporal]
+
+class SimilarityEdgeFilter(Modifier):
+    def __init__(self, *args, threshold=None, **kwargs):
         """
         Returns base_weight iff split_a and split_b are part of the same comment.
         :param args:
+        :threshold: value for edges to filter
         :param kwargs:
         """
         super().__init__(*args, **kwargs)
-        # fixme: do this in a way that mathces config files
-        self.thresholds = {models.EdgeType.SAME_COMMENT: 0.7,
-                           models.EdgeType.SAME_ARTICLE: 0.8,
-                           models.EdgeType.SIMILARITY: 0.9,
-                           models.EdgeType.SAME_GROUP: 0.1,
-                           models.EdgeType.REPLY_TO: 0.8}
-
-        logger.debug(f'{self.__class__.__name__} initialised')
-
-    @classmethod
-    def short_name(cls) -> str:
-        return 'ef'
-
-    def modify(self, graph: models.Graph) -> models.Graph:
-        def within_thresholds(wgt_list: List[models.EdgeWeightType], thresholds: Dict[models.EdgeType, float]):
-            for wgt in wgt_list:
-                if wgt[0] < thresholds[wgt[1]]:
-                    return False
-            return True
-
-        graph.edges = [edge for edge in graph.edges if within_thresholds(edge.wgts, thresholds=self.thresholds)]
-        return graph
-    # def remove_edges(self, textual=0.8, structural=0.96, temporal=7200):
-    #     # self.edges = [edge for edge in self.edges if edge.weights_bigger_as_threshold()]
-    #     self.edges = [edge for edge in self.edges
-    #                   if edge.weights["textual"] >= textual
-    #                   and edge.weights["structural"] >= structural
-    #                   and edge.weights["temporal"] <= temporal]
-
-
-class BottomEdgeFilter(Modifier):
-    def __init__(self, *args, base_weight=None, only_consecutive: bool = None, **kwargs):
-        """
-        Returns base_weight iff split_a and split_b are part of the same comment.
-        :param args:
-        :param base_weight: weight to attach
-        :param only_consecutive: only return weight of splits are consecutive
-        :param kwargs:
-        """
-        super().__init__(*args, **kwargs)
-        self.base_weight = self.conf_getfloat('base_weight', base_weight)
-        self.only_consecutive = self.conf_getboolean('only_consecutive', only_consecutive)
+        self.threshold = self.conf_getfloat("threshold", threshold)
 
         logger.debug(f'{self.__class__.__name__} initialised with '
-                     f'base_weight: {self.base_weight} and only_consecutive: {self.only_consecutive}')
+                     f'{self.threshold=}')
 
     @classmethod
     def short_name(cls) -> str:
-        return 'bef'
+        return 'sef'
 
-    def modify(self, graph: models.Graph, top_edges=5, edge_type=models.EdgeType.SIMILARITY) -> models.Graph:
+    @classmethod
+    def edge_type(cls) -> models.EdgeType:
+        return models.EdgeType.SIMILARITY
+
+    def modify(self, graph: models.Graph) -> models.Graph:
+        # FIXME: dont understand why edge_type is function call here and in BottomSimilarityEdgeFilter attribute
+        graph.edges = [edge for edge in graph.edges if edge.wgts[self.__class__.edge_type()] > self.threshold]
+        return graph
+
+
+class BottomSimilarityEdgeFilter(Modifier):
+    def __init__(self, *args,  top_edges=None, **kwargs):
+        """
+        Filters all edges except top edges for specific edge type
+        :param args:
+        :param top_edges: the number of top edges to keep for each node
+        :param kwargs:
+        """
+        super().__init__(*args, **kwargs)
+        self.top_edges = self.conf_getint('d', top_edges)
+
+        logger.debug(f'{self.__class__.__name__} initialised with '
+                     f'{self.top_edges=}')
+
+    @classmethod
+    def short_name(cls) -> str:
+        return 'bsef'
+
+    @classmethod
+    def edge_type(cls) -> models.EdgeType:
+        return models.EdgeType.SIMILARITY
+
+    def modify(self, graph: models.Graph) -> models.Graph:
         filtered_edges = []
         edge_dict = build_edge_dict(graph)
 
         # FIXME: check if node index correctly choosen
         for node_id in graph.id2idx.keys():
             node_edges = edge_dict[node_id]
-            node_edges = sorted(node_edges, key=lambda e: e.wgts[edge_type], reverse=True)[:top_edges]
+            node_edges = sorted(node_edges, key=lambda e: e.wgts[self.__class__.edge_type], reverse=True)[:self.top_edges]
             for edge in node_edges:
                 if edge not in filtered_edges:
                     filtered_edges.append(edge)
         graph.edges = filtered_edges
 
         return graph
+
+
+class BottomReplyToEdgeFilter(Modifier):
+    def __init__(self, *args,  top_edges=None, **kwargs):
+        """
+        Filters all edges except top edges for specific edge type
+        :param args:
+        :param top_edges: the number of top edges to keep for each node
+        :param kwargs:
+        """
+        super().__init__(*args, **kwargs)
+        self.top_edges = self.conf_getint('d', top_edges)
+
+        logger.debug(f'{self.__class__.__name__} initialised with '
+                     f'{self.top_edges=}')
+
+    @classmethod
+    def short_name(cls) -> str:
+        return 'brtef'
+
+    @classmethod
+    def edge_type(cls) -> models.EdgeType:
+        return models.EdgeType.REPLY_TO
+
+    def modify(self, graph: models.Graph) -> models.Graph:
+        filtered_edges = []
+        edge_dict = build_edge_dict(graph)
+
+        # FIXME: check if node index correctly choosen
+        for node_id in graph.id2idx.keys():
+            node_edges = edge_dict[node_id]
+            node_edges = sorted(node_edges, key=lambda e: e.wgts[self.__class__.edge_type], reverse=True)[:self.top_edges]
+            for edge in node_edges:
+                if edge not in filtered_edges:
+                    filtered_edges.append(edge)
+        graph.edges = filtered_edges
+
+        return graph
+
+# todo: add bottom edge filters for other edge types
 
 
 class Representives:
