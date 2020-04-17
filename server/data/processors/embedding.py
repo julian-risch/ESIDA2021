4dea194c73dbdb0366c98a160fafac966100ff3b
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-04-17 09:12:07 +0200

    improved vectorization

--- server/data/processors/embedding.py
+++ server/data/processors/embedding.py
@@ -1,15 +1,20 @@
 import numpy as np
 import fasttext as ft
+from typing import List, Dict
 
 
 def load_fasttext_model(path='E:\\cc.de.300.bin'):
     return ft.load_model(path)
 
 
-def vectorize(model, comment_texts):
+def vectorize_sentence(model, sentence: str) -> List[float]:
+    return model.get_sentence_vector(sentence.replace("\n", " "))
+
+
+def vectorize_comments(model, comment_texts: List[str]) -> Dict[int, List[float]]:
     if not model:
         model = load_fasttext_model()
-    vectorized_documents = {i: model.get_sentence_vector(comment_text.replace("\n", " "))
+    vectorized_documents = {i: vectorize_sentence(model, comment_text.replace("\n", " "))
                             for i, comment_text in enumerate(comment_texts)}
 
     vectorized_documents = {k: np.array(v) for k, v in vectorized_documents.items()}
