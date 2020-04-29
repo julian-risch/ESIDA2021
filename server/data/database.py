cbd76da2d283b7b972a05bb3ee030a5deb0318d8
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-04-29 09:31:40 +0200

    testing

--- server/data/database.py
+++ server/data/database.py
@@ -10,7 +10,7 @@ import data.models as models
 from common import config
 
 logger = logging.getLogger('data.db')
-
+print(config.get('cache', 'db_url'))
 DATABASE_URL = config.get('cache', 'db_url')
 
 database = databases.Database(DATABASE_URL)
